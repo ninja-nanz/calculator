980619b801a053948b0180d3386d12fddb5bcf12
Nanz Nair <nanz89@gmail.com>
2019-03-13 12:28:39 +1300

    updating styles, pseudocode for JS

---
+++ calculator.js
@@ -0,0 +1,29 @@
+// setup gobal variables
+
+
+// get the numbers and create an array 
+
+
+// cannot have multiple decimal points
+
+
+// cannot start with operators or more than one 0 , except "-"
+
+// cannot have multiple consecutive operators, use only the last operator clicked
+
+
+// clear the calculator 
+
+
+// create the equation from the array
+
+
+// calculate the equation 
+
+
+// use answer from previous equation for new equation 
+
+
+
+//display the result when "=" is clicked 
+
